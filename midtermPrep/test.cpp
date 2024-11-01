1fbd4e44c763b5bdfbd612604e3a37baa89cd42c
M7Tr0_ <mechanozver@gmail.com>
2024-11-01 23:40:12 +0800

    trying to figure out ointers

--- midtermPrep/test.cpp
+++ midtermPrep/test.cpp
@@ -1,31 +1,26 @@
-//Palindrome:
 #include <iostream>
+
 using namespace std;
 
-bool palindrome(const char x []){
-    int j = 0;
-    int k = strlen(x) - 1;
-    for(; j < strlen(x) / 2; j++, k--){
-        if(x[j] != x[k]){
-            return false;
-        }
-    }
-    return true;
-}
-
 int main() {
-    const int maxCharacters = 10;
-    char wholeLine[maxCharacters + 1];//+1 to accomodate the null terminator 
+    int n;
 
-    while(cin.getline(wholeLine, maxCharacters+1, '\n') || //cin.getline() automatically apend the null character
-    cin.gcount() == maxCharacters){
-        
-        cout << endl << "gcount: " << cin.gcount() << endl;
-        cout << endl << "wholeLine: " << wholeLine << endl;
-        cout << endl << "wholeLine strlen: " << strlen(wholeLine) << endl;
-        cout << endl << "wholeLine sizeof: " << sizeof(wholeLine) << endl;
-        cout << endl << "Terminating null: " << (wholeLine[cin.gcount() - 1] == '\0') << endl;
-        
-        cin.clear();//erases possible error flags raised during input
+    cout << "\nPleae input the size of the ladder and triangle n: "; cin >> n;
+    cout << endl;
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n - i; j++) cout << " ";
+        for(int j = 1; j <= i; j++) cout << "*";
+        cout << endl;
     }
+
+    cout << endl;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < i; j++) cout << " ";
+        for(int j = 0; j < 2 * (n - i - 1) + 1; j++) cout << "*";
+        cout << endl;
+    }
+
+    return 0;
 }
